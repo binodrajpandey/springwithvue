19f94193ec006b8906d53506bc92310a4ad2b578
binodrajpandey <binodraj.pandey@bebit.com>
2020-09-08 17:39:34 +0900

    Create a project

---
+++ backend/src/main/java/com/bebit/vuewithspringboot/VuewithspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bebit.vuewithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuewithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuewithspringbootApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bebit/vuewithspringboot/VuewithspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebit.vuewithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuewithspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
